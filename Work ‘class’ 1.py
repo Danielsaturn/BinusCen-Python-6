64952ae697ea26618f4619f5ce51fb249a200afa
Danielsaturn <danielbussiness2000@gmail.com>
2024-12-21 14:47:35 +0700

    Update Work ‘class’ 1.py

--- Work ‘class’ 1.py
+++ Work ‘class’ 1.py
@@ -2,35 +2,34 @@ class student:
     "Common base class for all student"
     stucount = 0
     
-    def __init__(self, name="student", grade=1):
+    def __init__(self, name="student", umur=1):
         self.name = name
-        self.grade = grade
+        self.umur = umur
         student.stucount += 1
         
     def displayCount(self):
         print("Total Student: %d" % student.stucount)
     
     def printstudent(self):
-        print("Name:", self.name, "\nGrade:", self.grade)
+        print("Name:", self.name, "\nGrade:", self.umur)
     
     # Getter Method Declaration
     def getstudent(self, parameterType):
         if parameterType == "Name":
             return self.name
         elif parameterType == "grade":
-            return self.grade
+            return self.umur
         else:
             return "Data Not Found"
     
     # Setter Method Declaration
-    def setstudent(self, name, grade):
+    def setstudent(self, name, umur):
         self.name = name
-        self.grade = grade
+        self.umur = umur
 
 student1 = student()
 
 user_name = input("Please enter your name: ")
-user_grade = input("Please enter your grade: ")
-
-student1.setstudent(user_name, user_grade)
+user_umur = input("Please enter your grade: ")
+student1.setstudent(user_name, user_umur)
 student1.printstudent()
